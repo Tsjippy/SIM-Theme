580c9b93e6ab0d357b08f7dbfc5614e1070069b2
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2022-12-19 09:26:18 +0100

    added: support for ajax search lite plugin

---
+++ php/search.php
@@ -0,0 +1,14 @@
+<?php
+namespace SIMTHEME;
+
+add_action('init', function(){
+    // replace the searchform with the ajax search lite
+    if ( shortcode_exists( 'wpdreams_ajaxsearchlite' ) ) {
+        add_filter( 'generate_navigation_search_output', function() {
+            return sprintf(
+                '<div class="navigation-search">%s</div>',
+                do_shortcode( '[wpdreams_ajaxsearchlite]' )
+            );
+        } );
+    }
+});
