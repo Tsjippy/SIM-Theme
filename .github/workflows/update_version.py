645bac14505a381b29965a150d65325f0db9a214
Tsjippy <ewaldha@hotmail.com>
2024-04-08 08:58:30 +0100

    added: darkmode classes

--- .github/workflows/update_version.py
+++ .github/workflows/update_version.py
@@ -37,10 +37,11 @@ try:
     newTotal    = total
 
     # Remove emty sections
-    for x in ["Added", "Changed", "Fixed"]:
+        # Remove emty sections
+    for x in ["Added", "Changed", "Fixed", "Updated"]:
         pattern = r'(### '+x+'[\s\S]*'
 
-        if(x != 'Fixed'):
+        if(x != 'Updated'):
             pattern = pattern+'?)###'
         else:
             pattern = pattern+')'
